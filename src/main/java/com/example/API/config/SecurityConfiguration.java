36c92d3e9e227f7c198ea8d425023b23a3279b06
Corentin <corenbla@gmail.com>
2021-11-09 15:52:10 +0100

    fix : oublie + tyo

--- src/main/java/com/example/API/config/SecurityConfiguration.java
+++ src/main/java/com/example/API/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.API.config;
 
+import com.example.API.security.JWTAuthentificationFilter;
+import com.example.API.security.JWTAuthorizationFilter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,11 +17,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/api/security/register").permitAll()
                 .antMatchers(HttpMethod.POST,"/api/security/login").permitAll()
                 .antMatchers(HttpMethod.POST,"/api/security/candidats").hasAnyAuthority("ROLE_ADMIN")
-                .anyRequest().authenticated();
-//                .and()
-//                .addFilter(new JWTAuthenficationFilter(authenticationManager()))
-//                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthentificationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     }
 }
